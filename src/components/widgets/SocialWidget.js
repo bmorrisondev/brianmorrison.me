4368211c063031a98a7e002467261cac031e477c
Brian Morrison II <brian@brianmorrison.me>
2020-03-22 18:13:33 -0500

    Social links open in new win now

--- src/components/widgets/SocialWidget.js
+++ src/components/widgets/SocialWidget.js
@@ -4,47 +4,47 @@ const SocialWidget = ({shouldIncludeDiscord}) => (
   <div className="social-widget">
     <h4>Main Channel</h4>
     <div>            
-      <a href="https://twitch.tv/brianmmdev">
-        <img alt="twitch-logo" src='/img/social/twitch-logo.png' target="_blank" />
+      <a href="https://twitch.tv/brianmmdev" target="_blank" rel="noopener noreferrer">
+        <img alt="twitch-logo" src='/img/social/twitch-logo.png'/>
         Twitch Channel
       </a>
     </div>
     <h4>Channel Updates & IRL</h4>
     <div>            
-      <a href="https://twitter.com/brianmmdev">
-        <img alt="twitter-logo" src='/img/social/twitter-logo.png' target="_blank" />
+      <a href="https://twitter.com/brianmmdev" target="_blank" rel="noopener noreferrer"> 
+        <img alt="twitter-logo" src='/img/social/twitter-logo.png' />
         Twitter (@brianmmdev)
       </a>
     </div>
     <div>            
-      <a href="https://www.instagram.com/brianmmdev/">
-        <img alt="instagram-logo" src='/img/social/insta-logo.png' target="_blank" />
+      <a href="https://www.instagram.com/brianmmdev/" target="_blank" rel="noopener noreferrer">
+        <img alt="instagram-logo" src='/img/social/insta-logo.png' />
         Instagram
       </a>
     </div>
     <h4>Message Me</h4>
     <div>            
-      <a href="https://m.me/brianmm02/">
-        <img alt="messenger-logo" src='/img/social/messenger-logo.png' target="_blank" />
+      <a href="https://m.me/brianmm02/" target="_blank" rel="noopener noreferrer">
+        <img alt="messenger-logo" src='/img/social/messenger-logo.png' />
         Messenger
       </a>
     </div>
     <div>            
-      <a href="https://wa.me/17083412229">
-        <img alt="whatsapp-logo" src='/img/social/whatsapp-logo.png' target="_blank" />
+      <a href="https://wa.me/17083412229" target="_blank" rel="noopener noreferrer">
+        <img alt="whatsapp-logo" src='/img/social/whatsapp-logo.png' />
         WhatsApp
       </a>
     </div>
     <h4>Others</h4>
     <div>            
-      <a href="https://www.facebook.com/brianmm02">
-        <img alt="facebook-logo" src='/img/social/facebook-logo.png' target="_blank" />
+      <a href="https://www.facebook.com/brianmm02" target="_blank" rel="noopener noreferrer">
+        <img alt="facebook-logo" src='/img/social/facebook-logo.png' />
         Facebook  
       </a>
     </div>
     <div>            
-      <a href="https://www.linkedin.com/in/brianmorrison2/">
-        <img alt="linkedin-logo" src='/img/social/linkedin-logo.png' target="_blank" />
+      <a href="https://www.linkedin.com/in/brianmorrison2/" target="_blank" rel="noopener noreferrer">
+        <img alt="linkedin-logo" src='/img/social/linkedin-logo.png' />
         LinkedIn
       </a>
     </div>
