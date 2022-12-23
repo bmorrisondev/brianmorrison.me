163a0506cc7aa3d8dfc84ee482e8f5c55f2bf6cc
Brian Morrison II <brian@brianmorrison.me>
2022-12-23 13:25:53 -0600

    test title fix

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -81,6 +81,7 @@ function DefaultLayout(props: Props) {
 
   const origin = "https://brianmorrison.me"
   const defaultDescription = "Personal blog of Brian Morrison II, full stack developer & content creator."
+  const titleHeader = `${pageTitle ? `${pageTitle} - ` : ""}Brian Morrison II`
 
   return (
     <Wrapper>
@@ -88,12 +89,12 @@ function DefaultLayout(props: Props) {
         <title>{pageTitle ? `${pageTitle} - ` : ""}Brian Morrison II</title>
         <meta property="og:image" content={ogImageUrl ? origin + ogImageUrl  : `${origin}/img/social.png`} />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Brian Morrison II" />
+        <meta property="og:title" content={titleHeader} />
         <meta property="og:description" content={description ? description.replace(/(<([^>]+)>)/ig, '') : defaultDescription} />
         <meta property="og:url" content={origin + location.path} />
         <meta property="og:site_name" content="Brian Morrison II" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content={`${pageTitle ? `${pageTitle} - ` : ""}Brian Morrison II`} />
+        <meta property="twitter:title" content={titleHeader} />
         <meta property="twitter:site" content="@brianmmdev" />
         <meta property="twitter:creator" content="@brianmmdev" />
         <meta property="twitter:description" content={description ? description.replace(/(<([^>]+)>)/ig, '') : defaultDescription} />
