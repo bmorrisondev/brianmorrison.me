e422dccb088e81ed9c39cdf66f8dac1b07adf8e4
Brian Morrison II <brian@brianmorrison.me>
2023-05-11 21:00:11 -0400

    brianmm.dev styling

--- brianmm.dev/src/app/page.tsx
+++ brianmm.dev/src/app/page.tsx
@@ -1,33 +1,51 @@
 import Image from 'next/image'
 import HomeCard from '../components/HomeCard'
 import ProfileImg from '../assets/profile.png'
+import YouTube from '@/components/svgs/YouTube'
+import Twitter from '@/components/svgs/Twitter'
+import LinkedIn from '@/components/svgs/LinkedIn'
+import Globe from '@/components/svgs/Globe'
+import Paper from '@/components/svgs/Paper'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col lg:gap-4 p-24">
+    <main className="flex min-h-screen flex-col lg:gap-4 lg:p-24 p-4 pt-24">
       <div className="z-10 w-full font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 
           flex w-full items-center justify-center gap-2 border-b
           border-gray-300 bg-gradient-to-b from-zinc-200 
-          py-1 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:pr-3 lg:dark:bg-zinc-800/30 text-lg">
-          <Image src={ProfileImg} alt="profile image" className="rounded-full h-[50px] w-auto" /> 
+          py-2 backdrop-blur-2xl lg:static lg:w-auto  
+          lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:pr-3 
+          lg:dark:bg-zinc-800/30 text-lg">
+          <img src="/img/profile.png" alt="profile image" className="rounded-full h-[50px] w-auto" /> 
           Brian Morrison II
         </p>
       </div>
+      <div className="mb-4 flex items-center justify-center lg:justify-start">
+        <span>Developer Educator @ <a href="https://planetscale.com" className="ml-1 border-b border-b-gray-400 hover:border-b-gray-700 transition">PlanetScale</a>.</span>
+      </div>
 
-      <div className="mb-32 grid lg:mb-0 xl:grid-cols-5 lg:grid-cols-4 text-left gap-4">
-        <HomeCard href="https://youtube.com/@brianmmdev" title="YouTube" color="red">
-          @brianmmdev
+      <div className="mb-4 grid lg:mb-0 xl:grid-cols-5 lg:grid-cols-4 grid-cols-2 text-left gap-4">
+        <HomeCard href="https://youtube.com/@brianmmdev" color="youtube">
+          <YouTube /> @brianmmdev
         </HomeCard>
-        <HomeCard href="https://twitter.com/@brianmmdev" title="Twitter" color="blue">
-          @brianmmdev
+        <HomeCard href="https://twitter.com/@brianmmdev" color="twitter">
+          <Twitter /> @brianmmdev
         </HomeCard>
-        <HomeCard href="https://twitter.com/@brianmmdev" title="LinkedIn">
-          @brianmmdev
+        <HomeCard href="https://www.linkedin.com/in/brianmmdev">
+          <LinkedIn /> @brianmmdev
         </HomeCard>
-        <HomeCard href="https://brianmorrison.me/blog" title="Blog" color="purple">
-          brianmorrison.me
+        <HomeCard href="https://brianmorrison.me/blog" color="purple">
+          <Paper /> my blog
         </HomeCard>
+      </div>
+
+      <div className='md:col-span-2 border-accent-2 border rounded-lg'>
+        <iframe src="https://embeds.beehiiv.com/db8ca15b-ac1c-4aa3-9912-46fec9df6688"
+          data-test-id="beehiiv-embed"
+          width="100%"
+          height="300"
+          className="rounded m-0 bg-transparent"></iframe>
       </div>
 
       {/* <div className="mb-32 grid lg:mb-0 xl:grid-cols-5 lg:grid-cols-4 text-left gap-4">
--- brianmm.dev/src/components/HomeCard.tsx
+++ brianmm.dev/src/components/HomeCard.tsx
@@ -2,15 +2,15 @@ import React, { ReactNode } from 'react'
 
 type Props = {
   href: string
-  title: string
+  title?: string
   children: ReactNode
   color?: string
 }
 
 const colors: any = {
   "default": "bg-gray-100/30 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
-  "red": "bg-gray-100/30 hover:border-red-300 hover:bg-red-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
-  "blue": "bg-gray-100/30 hover:border-blue-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
+  "youtube": "bg-gray-100/30 hover:border-red-300 hover:bg-red-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
+  "twitter": "bg-gray-100/30 hover:border-blue-300 hover:bg-blue-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
   "purple": "bg-gray-100/30 hover:border-purple-300 hover:bg-purple-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
 }
 
@@ -22,13 +22,12 @@ function HomeCard({href, title, children, color}: Props) {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <h2 className={`mb-3 text-2xl font-semibold`}>
-        {title}{' '}
-        {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          -&gt;
-        </span> */}
-      </h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      {title && (
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          {title}{' '}
+        </h2>
+      )}
+      <p className={`m-0 max-w-[30ch] text-sm opacity-50 flex gap-1 items-center`}>
         { children }
       </p>
     </a>
