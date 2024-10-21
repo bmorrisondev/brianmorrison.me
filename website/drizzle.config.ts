5b8c0982dd0e5f6bcad60d5184f0a54788c1c7c8
Brian Morrison II <brian@brianmorrison.me>
2024-10-20 19:28:56 -0500

    another fix

--- website/drizzle.config.ts
+++ website/drizzle.config.ts
@@ -8,7 +8,7 @@ console.log(process.env.DATABASE_URL)
 export default {
   schema: "./functions/db/schema.ts",
   out: "./drizzle",
-  driver: 'pg',
+  // driver: 'pg',
   dialect: "postgresql",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL as string,
