792e43f48d20d59d4236c72de1af27cc4c67e3af
Brian Morrison II <brian@brianmorrison.me>
2025-07-25 10:50:42 -0500

    chore: update YouTube and LinkedIn profile URLs to use consistent brianmmdev handle

--- website/app/socials.ts
+++ website/app/socials.ts
@@ -1,13 +1,13 @@
 const socials = {
   "twitch": "https://twitch.tv/brianmm02",
   "twitter": "https://twitter.com/brianmmdev",
-  "youtube": "https://www.youtube.com/channel/UCLx9EihBDfoJMncRWSZZoXg",
+  "youtube": "https://www.youtube.com/@brianmmdev",
   "instagram": "https://www.instagram.com/brianmmdev/",
   "github": "https://github.com/bmorrisondev",
   "facebook": "https://www.facebook.com/brianmm02",
   "messenger": "https://m.me/brianmm02/",
   "whatsapp": "https://wa.me/17083412229",
-  "linkedin": "https://www.linkedin.com/in/brianmorrison2",
+  "linkedin": "https://www.linkedin.com/in/brianmmdev",
   "discord": "https://www.learnbuildteach.com",
   "dev_to": "https://dev.to/brianmmdev",
   "hashnode": "https://hashnode.com/@brianmmdev/joinme"
