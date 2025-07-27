9c9ac6a4b9f8ce83d52b5c2da09704b38123d962
Brian Morrison II <brian@brianmorrison.me>
2025-07-27 08:51:42 -0500

    feat: add onClick handler to MobileNavLink to close mobile menu after navigation

--- website/app/components/MobileNavLink.tsx
+++ website/app/components/MobileNavLink.tsx
@@ -2,15 +2,22 @@
 import { Link } from '@remix-run/react'
 import { ReactNode } from 'react'
 
-type Props = {
+interface Props {
   children: ReactNode
   to: string
   className?: string
+  onClick?: () => void
 }
 
-function MobileNavLink({ children, to, className }: Props) {
+function MobileNavLink({ children, to, className, onClick }: Props) {
+  const handleClick = () => {
+    if (onClick) {
+      onClick()
+    }
+  }
+
   return (
-    <Link to={to} className={`text-white text-xl px-8 py-4 ${className}`}>
+    <Link to={to} className={`text-white text-xl px-8 py-4 ${className}`} onClick={handleClick}>
       { children }
     </Link>
   )
