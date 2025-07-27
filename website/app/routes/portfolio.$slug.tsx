e0db79cf39030e41f4443a61969c902934bf3174
Brian Morrison II <brian@brianmorrison.me>
2025-07-27 09:08:13 -0500

    feat: add skills used section to portfolio item pages with icons and tags

--- website/app/routes/portfolio.$slug.tsx
+++ website/app/routes/portfolio.$slug.tsx
@@ -5,9 +5,24 @@ import { PortfolioItem } from '~/models'
 import portfolioItems from '../content/notion/notionPortfolioItem.json'
 import { LoaderFunctionArgs } from '@remix-run/node'
 import { json, useLoaderData } from '@remix-run/react'
+import tags from '../content/notion/notionTag.json'
+import { Tag } from '~/models'
+import StylizedListItem from '~/components/StylizedListItem'
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const item: PortfolioItem = portfolioItems.find(el => el.slug === params.slug)
+
+  item.skillsUsed = []
+  item.relation_skillsUsed.forEach(sid => {
+    // TODO: This is ignored because `Tag` doesn't match the data.
+    // The fields are not named the same for some of the relations
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const tag = (tags as Tag[]).find(t => t.id === sid)
+    if(tag) {
+      item.skillsUsed.push(tag)
+    }
+      })
 
   return json({
     item
@@ -28,6 +43,20 @@ function PortfolioItemView() {
         <header>
           <h1 itemProp="headline">{item.title}</h1>
         </header>
+
+        {item.skillsUsed && item.skillsUsed.length > 0 && (
+          <div className="flex flex-col gap-2 mb-2">
+           <div className="font-bold">Created with:</div>
+           <div className="flex gap-2">
+            {item.skillsUsed.map((tag, index) => (
+              <StylizedListItem key={index} className="flex items-center gap-0.5">
+                <img key={`${item.notion_id}-${item.slug}`} src={tag.icon} alt={tag.name} className="max-w-[20px] max-h-[20px] border-none !m-0"/>
+                <div>{tag.name}</div>
+              </StylizedListItem>
+            ))}
+          </div>
+          </div>
+        )}
 
         {!!item.html && (
           <div className="post-content">{parse(item.html, { replace: replaceCode })}</div>
